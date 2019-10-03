37d5b1f314f549b60917526cbbb587ef3efe82a0
lesimon <simon.satoshi.rene.leisibach@cern.ch>
2019-10-03 11:18:01 +0200

    implement requested changes

--- ntpl005_h1analysis.C
+++ ntpl005_h1analysis.C
@@ -1,11 +1,24 @@
-//
-//  h1analysis.c
-//  
-//
-//  Created by Simon Leisibach on 25.09.19.
-//
+/// \file
+/// \ingroup tutorial_ntuple
+/// \notebook
+/// Multiple RNTuple files containing data from DESY are chained.
+/// This tutorial illustrates how multiple RNTuple files with identical fields can be chained and be treated
+/// like a single file.
+/// For reading, the tutorial shows the use of an ntuple View, which selectively accesses specific fields.
+/// If a view is used for reading, there is no need to load the dictionary for custom-made objects.
+/// The advantage of a view is that it directly accesses RNTuple's data buffers without making an additional
+/// memory copy. It also illustrates the usage of CollectionViews.
+///
+/// \macro_image
+/// \macro_code
+///
+/// \date October 2019
+/// \author The ROOT Team
 
-#include "convert.h"
+// NOTE: The RNTuple classes are experimental at this point.
+// Functionality, interface, and data format is still subject to changes.
+// Do not use for real data!
+
 #include "h1event.h"
 
 #include <ROOT/RNTuple.hxx>
@@ -21,12 +34,14 @@
 
 #include <cstdint>
 
+// Import classes from experimental namespace for the time being
 using RNTupleModel = ROOT::Experimental::RNTupleModel;
 using RNTupleReader = ROOT::Experimental::RNTupleReader;
 using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
 
 
 const std::string_view ntuplename = "eventdata";
+// assumes ntuple files already exist.
 const std::vector<std::string> ntupleFiles { "dstarmb.root", "dstarp1a.root", "dstarp1b.root", "dstarp2.root"};
 const Double_t dxbin = (0.17-0.13)/40;   // Bin-width
 const Double_t sigma = 0.0012;
@@ -53,23 +68,17 @@ Double_t fdm2(Double_t *xx, Double_t *par)
    return res;
 }
 
-void h1analysis()
+void ntpl005_h1analysis()
 {
-   // Always create ntuple files from TTree files from scratch because the ntuple file format may change.
-   for (const auto& f: ntupleFiles) {
-      convert(f);
-   }
-
-   // Ntuple fields can be only created for basic types (e.g. bool) and types with dictionaries.
-   gSystem->Load("./libH1event.so");
-
+   auto emptyModel = RNTupleModel::Create();
+   // Workaround: RNTuple should only lazily load the default model
    // Note here, that a std::vector of filenames (ntupleFiles) is passed here to chain multiple files.
-   auto ntuple = RNTupleReader::Open("h42", ntupleFiles);
-   ntuple->Show(10);
+   auto ntuple = RNTupleReader::Open(std::move(emptyModel), "h42", ntupleFiles);
 
    auto hdmd = new TH1F("hdmd","dm_d",40,0.13,0.17);
    auto h2   = new TH2F("h2","ptD0 vs dm_d",30,0.135,0.165,30,-3,6);
 
+   // Create a view for every field from which data is read.
    auto dm_dView = ntuple->GetView<float>("event.dm_d");
    auto rpd0_tView = ntuple->GetView<float>("event.rpd0_t");
    auto ptd0_dView = ntuple->GetView<float>("event.ptd0_d");
@@ -80,7 +89,8 @@ void h1analysis()
    auto ipiView = ntuple->GetView<std::int32_t>("event.ipi");
    auto ipisView = ntuple->GetView<std::int32_t>("event.ipis");
    auto md0_dView = ntuple->GetView<float>("event.md0_d");
-
+   
+   // CollectionViews allow to iterate through entries of its subfields
    auto trackView = ntuple->GetViewCollection("event.tracks");
    auto nhitrpView = ntuple->GetView<std::int32_t>("event.tracks.H1Event::Track.nhitrp");
    auto rstartView = ntuple->GetView<float>("event.tracks.H1Event::Track.rstart");
@@ -93,11 +103,15 @@ void h1analysis()
       auto ik = ikView(i) - 1;
       auto ipi = ipiView(i) - 1;
       auto ipis = ipisView(i) - 1;
-      
+
       if (TMath::Abs(md0_dView(i)-1.8646) >= 0.04) continue;
       if (ptds_dView(i) <= 2.5) continue;
       if (TMath::Abs(etads_dView(i)) >= 1.5) continue;
 
+      // nhitrpView(*trackView.GetViewRange(i).begin()) returns the value of the first element of the i-th entry.
+      // nhitrpView(i) would not do the same, because here a single entry contains a vector of elements. (In this
+      // case the field: "event.tracks" contains a std::vector of H1Event::Track, which is the parent field of
+      // "event.tracks.H1Event::Track.nhitrp")
       if (nhitrpView(*trackView.GetViewRange(i).begin()+ik) * nhitrpView(*trackView.GetViewRange(i).begin()+ipi) <= 1) continue;
       if (rendView(*trackView.GetViewRange(i).begin()+ik) - rstartView(*trackView.GetViewRange(i).begin()+ik) <= 22) continue;
       if (rendView(*trackView.GetViewRange(i).begin()+ipi) - rstartView(*trackView.GetViewRange(i).begin()+ipi) <= 22) continue;
@@ -119,6 +133,7 @@ void h1analysis()
    TF1 *f5 = new TF1("f5",fdm5,0.139,0.17,5);
    f5->SetParameters(1000000, .25, 2000, .1454, .001);
    hdmd->Fit("f5","lr");
+   hdmd->DrawCopy();
 
    gStyle->SetOptFit(0);
    gStyle->SetOptStat(1100);
@@ -136,12 +151,4 @@ void h1analysis()
    c2->Update();
    TLine *line = new TLine(0,0,0,c2->GetUymax());
    line->Draw();
-
-   c1->Print("c1.pdf");
-   c2->Print("c2.pdf");
-}
-
-int main() {
-   h1analysis();
-   return 0;
 }
